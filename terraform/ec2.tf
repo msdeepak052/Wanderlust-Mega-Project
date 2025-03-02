e22edc4d3ee429fe7267b70a0c9dff6e8ebbb813
msdeepak052 <160315954+msdeepak052@users.noreply.github.com>
2025-03-02 19:45:22 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,6 +6,8 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "Allow TLS"
   description = "Allow user to connect"
   vpc_id      = data.aws_vpc.default.id
+
+  # Allow SSH (port 22)
   ingress {
     description = "port 22 allow"
     from_port   = 22
@@ -14,14 +16,7 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # Allow HTTP (port 80)
   ingress {
     description = "port 80 allow"
     from_port   = 80
@@ -30,11 +25,75 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow HTTPS (port 443)
   ingress {
     description = "port 443 allow"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow Redis (port 6379)
+  ingress {
+    description = "Redis Caching"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow App Ports (3000 - 10000)
+  ingress {
+    description = "App Ports"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SMTPS (port 465)
+  ingress {
+    description = "Mail Transfer"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SMTP (port 25)
+  ingress {
+    description = "Simple Mail Transfer"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow Kubernetes API (port 6443)
+  ingress {
+    description = "k8s"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow Kubernetes Node Ports (30000 - 32767)
+  ingress {
+    description = "Kubernetes Node Ports"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress rule to allow all outgoing traffic
+  egress {
+    description = "allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
